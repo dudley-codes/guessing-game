921f842d1b45eea139dc738ac46282be01d053fc
dudley-codes <alexdudley@me.com>
2021-05-25 13:55:53 -0500

    Update[Program.cs]: User now has four chances to guess number

--- Program.cs
+++ Program.cs
@@ -4,30 +4,40 @@ GuessNum();
 
 void GuessNum()
 {
-    Console.Write("Can you guess the secret number?: ");
-    int answer;
-    bool success = false;
-    int SecretNumber = 42;
+  Console.Write("Can you guess the secret number?: ");
+  int answer;
+  bool success = false;
+  int SecretNumber = 42;
 
-    while (success == false)
+  while (success == false)
+  {
+    for (int i = 4; i > 0; i--)
     {
 
-    success = int.TryParse(Console.ReadLine(), out answer);
-    if(success)
-    {
-      if(SecretNumber == answer)
+      success = int.TryParse(Console.ReadLine(), out answer);
+      if (success)
       {
-      Console.WriteLine("You guessed correct!");
+        if (SecretNumber == answer)
+        {
+          Console.WriteLine("You guessed correct!");
+          break;
+        }
+        else
+        {
+          if (i > 1)
+          {
+            Console.Write("Wrong! Guess again: ");
+          }
+          else
+          {
+            Console.WriteLine("Sorry, you ran out of guesses");
+          }
+        }
       }
       else
       {
-        Console.WriteLine("Wrong. Guess again!");
+        Console.Write("Please enter a number: ");
       }
     }
-    else
-    {
-      Console.Write("Please enter a number: ");
-    }
-
-    }
+  }
 }
